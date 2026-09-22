9fdd5ab7d8c46bc1991d6deceb26f6616f37c4a5
Anant <anantdevsingh2009@gmail.com>
2026-09-22 21:48:55 +0530

    Update script.js

--- script (1).js
+++ script (1).js
@@ -1,1 +1,1 @@
-const menuBtn=document.getElementById("menuBtn");const nav=document.getElementById("nav");menuBtn.addEventListener("click",()=>nav.classList.toggle("open"));document.querySelectorAll("nav a").forEach(a=>a.addEventListener("click",()=>nav.classList.remove("open")));document.getElementById("year").textContent=new Date().getFullYear();
+const menuBtn=document.getElementById("menuBtn");const nav=document.getElementById("nav");menuBtn.addEventListener("click",()=>nav.classList.toggle("open"));document.querySelectorAll("nav a").forEach(a=>a.addEventListener("click",()=
